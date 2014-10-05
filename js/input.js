22c6109f511213625102cd4be6c3335c24daab99
hathix <neel@Neels-Mac.local>
2014-10-05 00:35:20 -0400

    Updated back to what i had

---
+++ js/input.js
@@ -0,0 +1,9 @@
+/**
+
+Returns the text the user wants to render, drawn from one of various sources.
+@return {String}    The text to render
+
+*/
+function getInput(){
+    return $('#inputText').val();
+}
